63f7b37afc3078056ecd601fda51a1f4a1ccbf44
hoang tien dng <dnghtien@gmail.com>
2020-03-17 18:56:12 +0700

    add Rest full API

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT||3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const postRoutes = require('./routes/api_route');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(express.urlencoded({
+    extended: false
+}));
+
+app.use(express.json());
+
+app.use('/', postRoutes);
+
+
+app.get('/', (req, res, next) => {
+    res.send('running node-api');
+});
+
+mongoose
+    .connect('mongodb+srv://dnghtien:zzdr10389@cluster0-jlgxs.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        app.listen( process.env.PORT, console.log('Server is running'));
+    })
+    .catch(err => console.log(err));
